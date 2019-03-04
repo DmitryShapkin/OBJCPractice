13fa3ba5a02245a95b65f2d9ff9f9122cd18fc6c
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-04 10:45:38 +0300

    Added Without Storyboards template

---
+++ Without Storyboards/Without Storyboards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Without Storyboards
+//
+//  Created by Dmitry Shapkin on 04/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
