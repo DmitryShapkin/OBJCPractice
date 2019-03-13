3daacc6b01223a9ba5d1806d0a2f33e67406e87f
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-13 21:25:46 +0300

    Initial commit

---
+++ MyOwnAppDelegate/MyOwnAppDelegate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyOwnAppDelegate
+//
+//  Created by Dmitry Shapkin on 13/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
+
