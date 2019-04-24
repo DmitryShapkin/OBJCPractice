6cdbc083effd8aa1aacfbe5d6e5fab63d2d991e1
Dmitry Shapkin <shapkindev@gmail.com>
2019-04-24 10:50:31 +0300

    Addec some comments to Blocks

--- Block Class.m
+++ Block Class.m
@@ -7,29 +7,42 @@
 
 /** Классы блоков */
 
-// __NSStackBlock__
+
+/**
+ __NSStackBlock__
+ Если захватываем внешние переменные
+ */
 int foo = 3;
 Class class = [^{
     int foo1 = foo + 1;
 } class];
 NSLog(@"%@", NSStringFromClass(class));
 
-// __NSMallocBlock__
+/**
+ __NSMallocBlock__
+ Когда используем "copy"
+ */
 int foo2 = 3;
 Class class2 = [[^{
     int foo1 = foo + 1;
 } copy] class];
 NSLog(@"%@", NSStringFromClass(class2));
 
-// __NSGlobalBlock__
+/**
+ __NSGlobalBlock__
+ Когда НЕ захватываем внешние переменные
+ */
 Class class3 = [^{
 } class];
 NSLog(@"%@", NSStringFromClass(class3));
 
-// __NSMallocBlock__
-int foo777 = 3;
+/**
+ __NSMallocBlock__
+ Вариант с ARC
+ */
+int foo3 = 3;
 id thisIsBlock = ^{
-    int foo1 = foo777 + 1;
+    int foo1 = foo3 + 1;
 };
 Class class4 = [thisIsBlock class];
 NSLog(@"%@", NSStringFromClass(class4));
